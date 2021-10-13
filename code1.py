d6d51d9c5065a7651a265c8ff11b21e25c78ded3
Shubhi <shubhi.paul2021@vitstudent.ac.in>
2021-10-13 11:42:02 +0530

    added to existing code

--- code1.py
+++ code1.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import random
 
 pygame.init()
 
@@ -17,14 +18,23 @@ red   = (255,0,0)
 blue  = (0,0,255)
 green = (0,255,0)
 
-def car(x,y):
+def playercar(x,y):
     gameDisplay.blit(player_car,(x,y))
+
+def start_text():
+    largetext = pygame.font.Font('freesansbold.ttf',25)
+    textSurf = largetext.render("Press any key to begin the game !", True, red)
+    textRect = textSurf.get_rect()
+    textRect.center=((screen_width/2 , screen_ht/5))
+    gameDisplay.blit(textSurf, textRect)
+    
+
 
 def gameLoop():
 
-    gameDisplay.fill(white)
-    x = screen_width * 0.45
-    y = screen_ht * 0.8
+    x_player = screen_width * 0.45
+    y_player = screen_ht * 0.8
+    x_change,y_change = 0,0
 
     gameQuit = False
 
@@ -33,7 +43,29 @@ def gameLoop():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        car(x,y)
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                if event.key == pygame.K_RIGHT:
+                    x_change = 5
+                if event.key == pygame.K_DOWN: 
+                    y_change = 5
+                if event.key == pygame.K_UP:
+                    y_change = -5
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_DOWN or event.key == pygame.K_UP:
+                    y_change = 0
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
+
+        x_player += x_change
+        y_player += y_change
+
+        gameDisplay.fill(white)
+
+        playercar(x_player,y_player)
+
         pygame.display.update()
         clock.tick(60)
        
@@ -42,3 +74,13 @@ def gameLoop():
 gameLoop()
 pygame.quit()
 quit()
+
+'''
+enemy_car = pygame.image.load('racecar2.png')
+
+def enemycar(x,y):
+    gameDisplay.blit(enemy_car,(x,y))
+
+y_enemy = screen_ht * 0.2 
+x_change, y_change = 0 ,0
+'''
